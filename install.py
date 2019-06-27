300f1b2aa20ccb9bc0389509d4c9a76561f54b39
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-06-27 15:28:07 +0200

    write config.ini

--- install.py
+++ install.py
@@ -26,39 +26,44 @@ os.system("""sudo apt-get install build-essential \
     python3-numpy \
     zip""")
 
-os.system("python3 -m venv %s/master_venv" % curpath)
+os.system("python3 -m venv '%s/master_venv'" % curpath)
 
 with open("%s/master_init.sh" % curpath, "w") as f:
     f.write("""#!/bin/sh
 
-. %s/bin/activate
+. '%s/master_venv/bin/activate'
 
 # avoid to stuck raspberry cpus
 export OPENBLAS_NUM_THREADS=1
 export OPENBLAS_MAIN_FREE=1
 
-cd %s/master
+cd '%s/master'
 python3 main.py &
 
-cd %s/master/slave
+cd '%s/master/slave'
 python3 main.py &
 """ % (curpath, curpath, curpath))
 
-os.system("chmod +x %s/master_init.sh" % curpath)
+os.system("chmod +x '%s/master_init.sh'" % curpath)
 
 if os.uname()[4][:3] == "arm":
-    os.system("python3 -m pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp35-cp35m-linux_armv7l.whl")
-    os.system("python3 -m pip install gpiozero")
-    os.system("python3 -m pip install picamera")
-    os.system("python3 -m pip install rpi.gpio")
-    
+    os.system(""". '%s/master_venv/bin/activate' && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp35-cp35m-linux_armv7l.whl && \
+pip install gpiozero && pip install picamera && pip install rpi.gpio""" % curpath)
+
     with open(os.expanduser("~/.profile"), "a") as f:
-        f.write("\n%s/master_init.sh\n" % curpath)
+        f.write("\n'%s/slave_init.sh'\n" % curpath)
 else:
-    os.system("python3 -m pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp36-cp36m-linux_x86_64.whl")
-    os.system("python3 -m pip install opencv-python")
+    os.system(""". '%s/master_venv/bin/activate' && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp36-cp36m-linux_x86_64.whl && \
+pip install opencv-python""" % curpath)
 
-os.system("python3 -m pip install -r requirements.txt")
+os.system(". '%s/master_venv/bin/activate' && pip install -r requirements.txt" % curpath)
 
-os.system("cd %s && git clone --recursive https://github.com/humanfirewall-iot19/master && cd master && git submodule update --remote" % curpath)
+os.system("cd '%s' && git clone --recursive https://github.com/humanfirewall-iot19/master && cd master && git submodule update --remote" % curpath)
+
+print("\n\n")
+telegram_tok = input("Please insert your Telegram Api Key:")
+
+with open("%s/master/config.ini" % curpath, "w") as f:
+    f.write("""[telegram]
+token = %s""" % telegram_tok)
 
